31a7812d24cc606485d1e54f9977678f66cbecae
mrscott52 <mrscott@smu.edu>
2016-09-28 15:56:37 -0500

    Update RunBothMotors.java

--- RunBothMotors.java
+++ RunBothMotors.java
@@ -10,7 +10,7 @@ public class RunBothEncodedMotors
 		r.connect(); 
 		//We don't have to attach anything because these motors are 
 		//attached by default 
-		r.runEncodedMotor(RXTXRobot.MOTOR1, 255, 100, RXTXRobot.MOTOR2, 255, 500); // Run both motors forward, one for 100,000 ticks and one for 50,000 ticks. 
+		r.runEncodedMotor(RXTXRobot.MOTOR1, 255, 100, RXTXRobot.MOTOR2, 255, 100); // Run both motors forward, one for 100,000 ticks and one for 100,000 ticks. 
 		r.close(); 
 	} 
 } 
