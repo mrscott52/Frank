fe34407d7fe8b87f30923a478aa0b35d14c64bf3
mrscott52 <mrscott@smu.edu>
2016-09-28 14:26:17 -0500

    Create TemperatureProbe.java

---
+++ TemperatureProbe.java
@@ -0,0 +1,33 @@
+//This function reads the voltage coming into analog pin 0 ten times
+//takes the average, then returns the result.
+public static int getThermistorReading() {
+ int sum = 0;
+ int readingCount = 10;
+
+ //Read the analog pin values ten times, adding to sum each time
+ for (int i = 0; i < readingCount; i++) {
+
+ //Refresh the analog pins so we get new readings
+ robot.refreshAnalogPins();
+ int reading = robot.getAnalogPin(0).getValue();
+ sum += reading;
+ }
+
+ //Return the average reading
+ return sum / readingCount;
+ }
+public static RXTXRobot robot;
+//Your main method, where your program starts
+public static void main(String[] args) {
+
+ //Connect to the arduino
+ robot = new ArduinoUno();
+ robot.setPort("COM3");
+ robot.connect();
+
+ //Get the average thermistor reading
+ int thermistorReading = getThermistorReading();
+
+ //Print the results
+ System.out.println("The probe read the value: " + thermistorReading);
+ System.out.println("In volts: " + (thermistorReading * (5.0/1023.0)));
